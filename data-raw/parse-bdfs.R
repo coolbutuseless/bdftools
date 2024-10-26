ff60f8d32fc0279a50528ee13c42d9fa8d2cf919
mike <mikefc@coolbutuseless.com>
2024-10-26 18:25:18 +1000

    move fonts to data-raw

--- data-raw/parse-bdfs.R
+++ data-raw/parse-bdfs.R
@@ -1,7 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
 
-bdf_files <- list.files("inst/fonts/", pattern = "bdf$", full.names = TRUE)
+bdf_files <- list.files("data-raw/fonts/", pattern = "bdf$", full.names = TRUE)
+bdf_files
 
 bdf_names <- basename(tools::file_path_sans_ext(bdf_files))
 bdf_names[bdf_names == 'unifont-16.0.01'] <- 'unifont'
